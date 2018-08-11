b543674209fcbc70c945f18f91efbc06a7380c54
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-08-11 16:01:37 +0900

    Refactor sbcr10.rb

--- sbcr10.rb
+++ sbcr10.rb
@@ -1,8 +1,12 @@
 require 'cgi'
 
-def parse(page_source)
-  dates = page_source.scan(/(\d+)年(\d+)月(\d+| \d+)日<br \/>/)
-  url_titles = page_source.scan(/^<a href="(.+?)">([^<].+?)<\/a><br \/>/)
+TITLE = 'WWW.SBCR.JP トピックス'
+URL = 'https://www.sbcr.jp/topics/'
+PAGE_SRC = `/usr/local/bin/wget -q -O- https://www.sbcr.jp/topics/`
+
+def parse(page_src)
+  dates = page_src.scan(/(\d+)年(\d+)月(\d+| \d+)日<br \/>/)
+  url_titles = page_src.scan(/^<a href="(.+?)">([^<].+?)<\/a><br \/>/)
   url_titles.zip(dates).map do |url_title, ymd|
     url, title = *url_title
     [CGI.unescapeHTML(url), CGI.unescapeHTML(title), Time.local(*ymd)]
@@ -18,8 +22,4 @@ def format_text(title, url, url_title_time_ary)
   s
 end
 
-TITLE = 'WWW.SBCR.JP トピックス'
-URL = 'https://www.sbcr.jp/topics/'
-page_source = `/usr/local/bin/wget -q -O- https://www.sbcr.jp/topics/`
-
-puts format_text(TITLE, URL, parse(page_source))
+puts format_text(TITLE, URL, parse(PAGE_SRC))
